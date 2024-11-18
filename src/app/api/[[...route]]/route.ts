97e7826874679c302056a3dc1865bbddc47bdff6
Jappreet Singh <jappreets4747@gmail.com>
2024-11-18 15:01:11 +0530

    Hono Js set up and auth implementation

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,25 @@
+import { Context, Hono } from "hono";
+import { handle } from "hono/vercel";
+import {AuthConfig, initAuthConfig} from "@hono/auth-js"
+import authConfig from "@/auth.config";
+
+import userRoutes from "@/app/api/[[...route]]/user"
+export const runtime = "nodejs"
+
+
+function getAuthConfig(c: Context): AuthConfig {
+    return {
+        secret: process.env.AUTH_SECRET,
+        ...authConfig
+    }
+}
+const app = new Hono().basePath('/api')
+const routes = app.route('/user', userRoutes);
+app.use("*", initAuthConfig(getAuthConfig))
+
+export const GET = handle(app);
+export const POST = handle(app);
+export const PATCH = handle(app);
+export const DELETE = handle(app);
+
+export type AppType = typeof routes
