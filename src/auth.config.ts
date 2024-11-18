5adf10b2e2e0ea3335e48291fb629a88bfa99a5b
Jappreet Singh <jappreets4747@gmail.com>
2024-11-18 12:24:49 +0530

    Authenticated the app

---
+++ src/auth.config.ts
@@ -0,0 +1,9 @@
+import { NextAuthConfig } from "next-auth";
+import GitHub from "next-auth/providers/github";
+
+export default {
+    providers: [GitHub],
+    session: {
+        strategy: "jwt",
+    }
+} satisfies NextAuthConfig;
