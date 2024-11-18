2fadb3369bc314f524bd64c43d1faa76d7061369
Jappreet Singh <jappreets4747@gmail.com>
2024-11-18 14:01:23 +0530

    DB connection with the schema

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,7 +1,10 @@
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { NextAuthConfig } from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { db } from "./db/db";
 
 export default {
+    adapter: DrizzleAdapter(db),
     providers: [GitHub],
     session: {
         strategy: "jwt",
